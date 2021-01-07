0576bbc0945528e20cafd079dbf106df8dd9caf5
Difei Zhao <zdf1997121@gmail.com>
2021-01-07 19:00:00 +0800

    readme

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-=======
 # mikan2down
 
 This is an RSS scanner (for now it is exclusively for Mikan project) to check the feed regularly and retrive the targeted file url to aria2-like downloader through RPC (xml in this case).
--- debug.py
+++ debug.py
@@ -1,5 +1,9 @@
-import datetime
-now = datetime.datetime.now().strftime("%Y-%m")
-time = datetime.datetime.now().timetuple()
-today = now +"-"+ str(time.tm_mday)
-print(today)
+import xmlrpc.client
+
+def download(torrent):
+     #port 16800 is for Motrix, the default port for aria2 is 6800
+     s = xmlrpc.client.ServerProxy('http://localhost:16800/rpc')
+     s.aria2.addUri([torrent])
+
+torrent = 'https://mikanani.me/Download/20201228/da6e6b6155787f317ca943a066be671c7244e8eb.torrent'
+download(torrent)

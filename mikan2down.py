ee4907578223a1f18276734c97bafe7c24f625c8
Difei Zhao <zdf1997121@gmail.com>
2021-01-07 18:59:28 +0800

    minimum viable

--- mikan2down.py
+++ mikan2down.py
@@ -1,6 +1,6 @@
 import feedparser
 import ssl
-import requests
+#import requests
 import xmlrpc.client
 import re
 from lxml import etree
@@ -12,9 +12,23 @@ today = now +"-"+ str(time.tm_mday)
 
 def download(torrent):
      #port 16800 is for Motrix, the default port for aria2 is 6800
-     s = xmlrpclib.ServerProxy('http://localhost:16800/rpc')
+     s = xmlrpc.client.ServerProxy('http://localhost:16800/rpc')
      s.aria2.addUri([torrent])
 
+ssl._create_default_https_context = ssl._create_unverified_context
+rss_mikan = feedparser.parse('https://mikanani.me/RSS/Bangumi?bangumiId=2335&subgroupid=92')
+
+
+for item in rss_mikan['entries']:
+     date = re.split(r'T',item['published'])[0]
+     lang = re.split(r'\[',item['id'])[3][0]
+     if date == today and lang == '简':
+          torrent = rss_mikan['entries'][0]['links'][2]['href']
+          download(torrent)
+
+
+
+#discarded requests method
 '''
 info = requests.get('https://mikanani.me/Home/Bangumi/2335')
 selector = etree.HTML(info.text)
@@ -37,31 +51,4 @@ if date = time.strftime("%Y/%m/%d") and lang = '简':
      download(magnet)
 
 #2020/12/28 04:14
-
-
-'''
-ssl._create_default_https_context = ssl._create_unverified_context
-rss_mikan = feedparser.parse('https://mikanani.me/RSS/Bangumi?bangumiId=2335&subgroupid=92')
-
-print(re.split(r'\[',item['id'])[3][0])
-
-for item in rss_mikan['entries']:
-     date = item['published'][0:9]
-     lang = re.split(r'\[',item['id'])[3][0]
-     if date = today and lang = '简':
-          torrent = print(rss_mikan['entries'][0]['links'][2]['href'])
-          download(torrent)
-
-
-
-title = rss_mikan['entries'][0]['links']
-
-
-#print(rss_mikan['entries'][1]['link'])
-
-'''
-for entry in rss_mikan['entries']:
-     print(entry['title'])
-     print(entry['link'])
-     print(entry['published'])
 '''

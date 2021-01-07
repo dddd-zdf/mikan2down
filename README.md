ab081bf0c144355bcd6dc9854fc6d9bdea935cec
Difei Zhao <zdf1997121@gmail.com>
2021-01-07 17:25:50 +0800

    readme

--- README.md
+++ README.md
@@ -6,12 +6,6 @@
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
 \pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-# up2down\
-Check cyclically and retrieve url from web automatically to download (RSS-downloader-like)\
-\
-This is an RSS-like url retriever to send url through JSON-RPC to download. It can use calendar.app to launch itself regularly.}
 =======
 # mikan2down
 
@@ -20,4 +14,3 @@ This is an RSS scanner (for now it is exclusively for Mikan project) to check th
 Self-trigger can be implemented by Calendar.app. 
 
 This project is under MIT License.
->>>>>>> e70cd0f637e35c3190ab4d97131895d18d3994a1
--- mikan2down.py
+++ mikan2down.py
@@ -4,25 +4,57 @@ import requests
 import xmlrpc.client
 import re
 from lxml import etree
-import time
+import datetime
 
+now = datetime.datetime.now().strftime("%Y-%m")
+time = datetime.datetime.now().timetuple()
+today = now +"-"+ str(time.tm_mday)
 
+def download(torrent):
+     #port 16800 is for Motrix, the default port for aria2 is 6800
+     s = xmlrpclib.ServerProxy('http://localhost:16800/rpc')
+     s.aria2.addUri([torrent])
 
-
+'''
 info = requests.get('https://mikanani.me/Home/Bangumi/2335')
 selector = etree.HTML(info.text)
-date = selector.xpath("//*[@id='sk-container']/div[2]/table[8]/tbody/tr[1]/td[3]/text()")[0]
+
+content = selector.xpath("//*[@id='sk-container']/div[2]/table[8]/tbody/")
+
+
+print(content)
+
+#retrieve the first two items
+
+date = selector.xpath("//*[@id='sk-container']/div[2]/table[8]/tbody/tr[1]/td[3]/text()")[0][0:10]
 
 title = selector.xpath("//*[@id='sk-container']/div[2]/table[8]/tbody/tr[1]/td[1]/a[1]/text()")[0]
 lang = re.split(r'\[',title)[3][0]
 
-print(time.strftime("%Y/%m/%d"))
+
+if date = time.strftime("%Y/%m/%d") and lang = '简':
+     magnet = selector.xpath('//*[@id="sk-container"]/div[2]/table[8]/tbody/tr[1]/td[1]/a[2]/@data-clipboard-text')
+     download(magnet)
+
 #2020/12/28 04:14
 
-#magnet = selector.xpath('//*[@id="sk-container"]/div[2]/table[8]/tbody/tr[1]/td[1]/a[2]/@data-clipboard-text')
 
+'''
 ssl._create_default_https_context = ssl._create_unverified_context
 rss_mikan = feedparser.parse('https://mikanani.me/RSS/Bangumi?bangumiId=2335&subgroupid=92')
+
+print(re.split(r'\[',item['id'])[3][0])
+
+for item in rss_mikan['entries']:
+     date = item['published'][0:9]
+     lang = re.split(r'\[',item['id'])[3][0]
+     if date = today and lang = '简':
+          torrent = print(rss_mikan['entries'][0]['links'][2]['href'])
+          download(torrent)
+
+
+
+title = rss_mikan['entries'][0]['links']
 
 
 #print(rss_mikan['entries'][1]['link'])
